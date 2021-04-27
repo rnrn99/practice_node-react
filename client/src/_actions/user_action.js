463655936f0105078a798292f948b9fc241d6994
giu0213@naver.com <giu0213@naver.com>
2021-04-27 17:45:43 +0900

    register 기능 구현

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,15 +1,27 @@
 import axios from 'axios';
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from './types';
 
 export function loginUser(dataTosubmit) {
     
     const request = axios.post('/api/users/login', dataTosubmit)
-    .then(response => response.data)
+    .then(response => response.data);
 
     return {
         type: LOGIN_USER,
+        payload: request
+    }
+}
+
+export function registerUser(dataTosubmit) {
+    
+    const request = axios.post('/api/users/register', dataTosubmit)
+    .then(response => response.data);
+
+    return {
+        type: REGISTER_USER,
         payload: request
     }
 }
