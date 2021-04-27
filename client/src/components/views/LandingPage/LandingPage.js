cbf0754f6b98a9089c744965131b6cad7d46fd91
giu0213@naver.com <giu0213@naver.com>
2021-04-27 18:11:29 +0900

    reactstrap 적용 & logout 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,28 @@
 import React, {useEffect} from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
     
     useEffect(() => {
         axios.get('/api/hello').then(response => console.log(response.data));
     }, []);
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if(response.data.success) props.history.push('/login');
+                else alert("Failed to logout");
+            });
+    }
     
     
     return (
-        <div>
-            LandingPage
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+            
+            <Button color="primary" onClick={onClickHandler}>로그아웃</Button>
+
         </div>
     );
 }
